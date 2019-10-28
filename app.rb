809f35496485f842d0b7419ce6edf315f3e1088e
Jenny Chu <jchudesign0930@gmail.com>
2019-10-28 22:33:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
-class Application < Sinatra::Base
-  # Write your code here!
-
+class Application < Sinatra::Base # this is the main application file, it helps to transform a ruby class into a sinatra controller
+  get '/' do #
+    erb :index #tells the application to render the ERB templates in View
+ post '/greet' do
+    erb :greet
+  end
 end
